c371b7fdb3ebb251261b3849dc69770154a23cfc
Meera Unnikrishnan <meeraunnikrishnan25@gmail.com>
2022-11-05 17:03:19 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+// console.log("hello from Node app");
+// var passenger = {
+//     name: "passenger1",
+//     email: "passenger1@gmail.com",
+//     age: 22,
+//     phone:[1234,123456]
+
+// };
+// var data=JSON.stringify(passenger);
+// console.log(data);
+
+// output:{"name":"passenger1","email":"passenger1@gmail.com","age":22,"phone":[1234,123456]}
+
+// var text='{"name":"passenger1","email":"passenger1@gmail.com","age":22,"phone":[1234,123456]}'
+// var data=JSON.parse(text);
+// console.log(data);
+
+// output:{
+//     name: 'passenger1',
+//     email: 'passenger1@gmail.com',
+//     age: 22,
+//     phone: [ 1234, 123456 ]
+//   }
